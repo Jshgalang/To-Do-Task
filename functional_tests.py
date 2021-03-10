fbcdcf5eb979a6c8d0c0e7bf51187e52c6ba41de
isaac-gocuyo <isaac.gocuyo.official@gmail.com>
2021-03-10 08:45:59 -0500

    Pretty much done!

--- functional_tests.py
+++ functional_tests.py
@@ -17,7 +17,7 @@ class MikeTest(unittest.TestCase):
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_test, [row.text for row in rows])
+        self.assertIn(row_text, [row.text for row in rows])
 
 
     def test_start_a_list_and_retrieve_it_later(self):
--- lists/tests.py
+++ lists/tests.py
@@ -19,15 +19,37 @@ class HomePageTest(TestCase):
 		# self.assertTrue(html.strip().endswith('</html>'))
 		# expected_html = render_to_string("home.html")
 		# self.assertEqual(html,expected_html)
+		# code smell
 
 	def test_uses_home_page_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
 
 	def test_can_save_a_POST_request(self):
-		response = self.client.post('/', data={'item_text': 'A new list item'}) # form data to send
-		self.assertIn('A new list item', response.content.decode())
-		self.assertTemplateUsed(response, 'home.html')
+		self.client.post('/', data={'item_text': 'A new list item'}) # form data to send
+
+		self.assertEqual(Item.objects.count(), 1) # short for objects.all().count()
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+		# self.assertIn('A new list item', response.content.decode())
+		# self.assertTemplateUsed(response, 'home.html')
+
+	def test_only_saves_items_when_needed(self):
+		self.client.get('/')
+		self.assertEqual(Item.objects.count(),0)
+
+	def test_redirects_after_POST(self):
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+	def test_displays_all_list_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+		response = self.client.get('/')
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -47,7 +69,6 @@ class ItemModelTest(TestCase):
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
 		self.assertEqual(second_saved_item.text, 'Item the secoond')
 		
-
 
 
 """
