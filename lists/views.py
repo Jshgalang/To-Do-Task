a369ef592fee0994253a4f7d97c58f2beaa923bc
jshgalang <galang.joshaaron@gmail.com>
2021-03-17 20:58:02 +0800

    Please enter the commit message for your changes. Lines starting

--- lists/views.py
+++ lists/views.py
@@ -5,6 +5,7 @@ from lists.models import Item
 # Create your views here.
 # home_page = None
 
+# homepage controller
 def home_page(request):
 	# return HttpResponse('<html>')
 	# return HttpResponse('<html><title>To-Do lists</title>')
@@ -12,11 +13,14 @@ def home_page(request):
 	# if request.method == 'POST':
 	# 	return HttpResponse(request.POST['item_text'])
 
-	if request.method == 'POST':
-		# new_item_text = request.POST['item_text']
-		# Item.objects.create(text=new_item_text)
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/')
+	# # not needed because it has delegated its own controller
+	# if request.method == 'POST':
+	# 	# new_item_text = request.POST['item_text']
+	# 	# Item.objects.create(text=new_item_text)
+	# 	Item.objects.create(text=request.POST['item_text'])
+	# 	# return redirect('/')
+	# 	return redirect('/lists/the-only-list-in-the-world/')
+	
 	# else:
 	# 	new_item_text = ''
 
@@ -24,5 +28,16 @@ def home_page(request):
 	# item.text = request.POST.get('item_text', '')
 	# item.save() 
 	# return render(request, 'home.html', {'new_item_text': new_item_text, })
+	
+	# items = Item.objects.all()
+	# return render(request, 'home.html', {'items': items})
+	return render(request, 'home.html')
+
+# list controller
+def view_list(request):
 	items = Item.objects.all()
-	return render(request, 'home.html', {'items': items})
+	return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+	Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/the-only-list-in-the-world/')
