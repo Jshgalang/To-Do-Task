47de561ca5058e24d71e1b101a727c1b2f4ccf25
jshgalang <galang.joshaaron@gmail.com>
2021-03-18 20:16:49 +0800

    New lists test implementation

--- lists/models.py
+++ lists/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
 	text = models.TextField(default='')
+	list = models.ForeignKey('List', default=None)
+
+class List(models.Model): # # research more about the magic in models.Model why it can be instantiated automatically
+	pass
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -39,5 +39,6 @@ def view_list(request):
 	return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-	Item.objects.create(text=request.POST['item_text'])
+	list_ = List.objects.create()
+	Item.objects.create(text=request.POST['item_text'], list=list_)
 	return redirect('/lists/the-only-list-in-the-world/')
