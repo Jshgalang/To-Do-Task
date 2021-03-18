c22eb101378dc050ae0d124d97e9337f51afdb9a
jshgalang <galang.joshaaron@gmail.com>
2021-03-18 18:17:04 +0800

    adding new unit test flow and before HW implementation

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
-from lists.models import Item
+# from lists.models import Item
+from lists.models import Item, List
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
@@ -67,23 +68,34 @@ class HomePageTest(TestCase):
 	# 	self.assertIn('itemey 1', response.content.decode())
 	# 	self.assertIn('itemey 2', response.content.decode())
 
-class ItemModelTest(TestCase):
+# class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
 		first_item = Item()
 		first_item.text = 'The first (ever) list item'
+		first_item.list = list_
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = 'Item the second'
+		second_item.list = list_
 		second_item.save()
-		saved_items = Item.objects.all()
 
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
+
+		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
 
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(first_saved_item.list, list_)
 		self.assertEqual(second_saved_item.text, 'Item the second')
+		self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 	def test_uses_list_template(self):
