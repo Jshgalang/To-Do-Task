97dbb9298763f755971521567a8bd515980b8dea
jshgalang <galang.joshaaron@gmail.com>
2021-03-24 20:40:15 +0800

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -16,6 +16,29 @@ class MikeTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def test_layout_and_styling(self):
+		# Mike goes to homepage
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+		# Mike sees input box at the center
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+		 	512, 
+		 	delta = 10,
+		 	)
+
+		# mike starts a new list and sees it's also centered
+		inputbox.send_keys('testing')		
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: testing')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+		 	512, 
+		 	delta = 10,
+		 	)
+
 	# helper method
 	# def check_for_row_in_list_table(self, row_text):
 	# 	table = self.browser.find_element_by_id('id_list_table')
