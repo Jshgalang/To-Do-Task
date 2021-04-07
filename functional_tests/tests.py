4435711bf81b234ca1be167843c63f55e927c9ef
jshgalang <galang.joshaaron@gmail.com>
2021-04-07 19:41:28 +0800

    base.html inheritance

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,14 @@
 import unittest
 import time
-from django.test import LiveServerTestCase
+# from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
 MAX_WAIT = 10 # catch random glitches/ random slowdowns
 
-class MikeTest(LiveServerTestCase):
+class MikeTest(StaticLiveServerTestCase):
 	@classmethod
 	def setUp(self):
 		self.browser = webdriver.Firefox()
