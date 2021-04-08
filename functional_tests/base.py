fc8a297178cf5248faeb1a16e738c2c239b2e785
jshgalang <galang.joshaaron@gmail.com>
2021-04-08 21:08:03 +0800

    file refactoring of Fts and implements test list validation

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -33,3 +33,21 @@ class FunctionalTest(StaticLiveServerTestCase):
 				if time.time() - start_time > MAX_WAIT:
 					raise e
 				time.sleep(0.5)
+
+	def wait_for(self, fn):
+		# "every time you cause a reload, wait"
+		start_time = time.time()
+		while True:  # we're adding a polling logic to prev. check_for_row...
+			try:
+				# table = self.browser.find_element_by_id('id_list_table')
+				# # table = self.browser.find_element_by_id('id_nothing')
+				# rows = table.find_elements_by_tag_name('tr')
+				# self.assertIn(row_text, [row.text for row in rows])
+				# self.assertIn('foo', [row.text for row in rows])
+				return fn()
+			except (AssertionError, WebDriverException) as e:
+			# AssertionError when the row asserted is still missing
+			# WebdriverException if page is not loaded and selenium element is not loaded
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -10,22 +10,39 @@ from .base import FunctionalTest
 
 # MAX_WAIT = 10 # catch random glitches/ random slowdowns
 
-
 class ItemValidationTest(FunctionalTest):
     # cantaddemptylistitems
     # @skip
     def test_cannot_add_empty_list_items(self):
         # Mike goes to the home page and accidentally tries to submit
         # an empty list item. He hits ENTER on the empty input box
-
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # Home page refreshes, then error message pops up
         # saying list items cannot be blank
+        self.wait_for(lambda: self.assertEqual(
+        	self.browser.find_element_by_css_selector('.has-error').text, 
+        	"You can't have an empty list item."
+        ))
 
         # Mike tries again with some text for items, which now works
-
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy Milk')
+        
         # Then he decides to submit a second blank list item
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
         # He should receive a similar warning sa list page
+        self.wait_for(lambda: self.assertEqual(
+        	self.browser.find_element_by_css_selector('.has-error').text, 
+        	"You can't have an empty list item."
+        ))
 
         # Tapos icocorrect nya ulit by filling some text in
-        self.fail("Write me!")
+        self.browser.find_element_by_id('id_new_item').send_keys('Drink Red Horse Ice Cold')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy Milk')
+        self.wait_for_row_in_list_table('2: Drink Red Horse Ice Cold')
+        
