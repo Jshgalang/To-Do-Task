5a9906221c1cafaa149234e78ae911c2f187d96d
jshgalang <galang.joshaaron@gmail.com>
2021-04-09 21:01:22 +0800

    Refactor list view to handle new item POSTs

--- lists/tests/test_view.py
+++ lists/tests/test_view.py
@@ -140,6 +140,24 @@ class ListViewTest(TestCase):
 		response = self.client.get(f'/lists/{correct_list.id}/')
 		self.assertEqual(response.context['list'], correct_list)
 
+	def test_can_save_a_POST_request_to_an_existing_list(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		self.client.post(f'/lists/{correct_list.id}/', data={'item_text': 'A new item for the created list'})
+		
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new item for the created list')
+		self.assertEqual(new_item.list, correct_list)
+
+	def test_POST_redirects_to_list_view(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		response = self.client.post(f'/lists/{correct_list.id}/', data={'item_text': 'A new item for the created list'})
+		self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
 
 class NewListTest(TestCase):
 	def test_can_save_a_POST_request(self):
@@ -171,21 +189,21 @@ class NewListTest(TestCase):
 		self.assertEqual(List.objects.count(), 0)
 		self.assertEqual(Item.objects.count(), 0)
 
-class NewItemTest(TestCase):
-	def test_can_save_a_POST_request_to_an_existing_list(self):
-		other_list = List.objects.create()
-		correct_list = List.objects.create()
+# class NewItemTest(TestCase):
+	# def test_can_save_a_POST_request_to_an_existing_list(self):
+	# 	other_list = List.objects.create()
+	# 	correct_list = List.objects.create()
 
-		self.client.post(f'/lists/{correct_list.id}/add_item', data={'item_text': 'A new item for the created list'})
+	# 	self.client.post(f'/lists/{correct_list.id}/add_item', data={'item_text': 'A new item for the created list'})
 		
-		self.assertEqual(Item.objects.count(), 1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new item for the created list')
-		self.assertEqual(new_item.list, correct_list)
+	# 	self.assertEqual(Item.objects.count(), 1)
+	# 	new_item = Item.objects.first()
+	# 	self.assertEqual(new_item.text, 'A new item for the created list')
+	# 	self.assertEqual(new_item.list, correct_list)
 
-	def test_redirects_to_list_view(self):
-		other_list = List.objects.create()
-		correct_list = List.objects.create()
+	# def test_redirects_to_list_view(self):
+	# 	other_list = List.objects.create()
+	# 	correct_list = List.objects.create()
 
-		response = self.client.post(f'/lists/{correct_list.id}/add_item', data={'item_text': 'A new item for the created list'})
-		self.assertRedirects(response, f'/lists/{correct_list.id}/')
+	# 	response = self.client.post(f'/lists/{correct_list.id}/add_item', data={'item_text': 'A new item for the created list'})
+	# 	self.assertRedirects(response, f'/lists/{correct_list.id}/')
--- lists/views.py
+++ lists/views.py
@@ -38,6 +38,9 @@ def home_page(request):
 def view_list(request, foo):
 	# items = Item.objects.all()
 	list_ = List.objects.get(id=foo)
+	if request.method == 'POST':
+		Item.objects.create(text=request.POST['item_text'], list=list_)
+		return redirect(f'/lists/{list_.id}/')
 	return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
@@ -53,7 +56,7 @@ def new_list(request):
 		return render(request, 'home.html', {"error": error})
 	return redirect(f'/lists/{list_.id}/')
 
-def add_item(request, foo):
-	list_ = List.objects.get(id=foo)
-	Item.objects.create(text=request.POST['item_text'], list=list_)
-	return redirect(f'/lists/{list_.id}/')
+# def add_item(request, foo):
+# 	list_ = List.objects.get(id=foo)
+# 	Item.objects.create(text=request.POST['item_text'], list=list_)
+# 	return redirect(f'/lists/{list_.id}/')
