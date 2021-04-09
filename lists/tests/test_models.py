c44f99ed8911bb497a58b1a76fa0ed7c9502190c
isaac-gocuyo <isaac.gocuyo.official@gmail.com>
2021-04-09 07:31:07 -0400

    Split out unit tests into two files

---
+++ lists/tests/test_models.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from lists.models import Item, List
+
+
+# class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
+	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.list = list_
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.list = list_
+		second_item.save()
+		
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
+
+
+		saved_items = Item.objects.all()
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+
+		self.assertEqual(first_saved_item.list, list_)
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(first_saved_item.list, list_)
+		self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+"""
+# class SmokeTest(TestCase):
+	# def test_bad_math(self):
+		# self.assertEqual(1+1, 4)
+# Create your tests here.
+"""
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -55,36 +55,6 @@ class HomePageTest(TestCase):
 		self.assertIn('itemey 2', response.content.decode())
 	"""
 
-# class ItemModelTest(TestCase):
-class ListAndItemModelsTest(TestCase):
-	def test_saving_and_retrieving_items(self):
-		list_ = List()
-		list_.save()
-
-		first_item = Item()
-		first_item.text = 'The first (ever) list item'
-		first_item.list = list_
-		first_item.save()
-
-		second_item = Item()
-		second_item.text = 'Item the second'
-		second_item.list = list_
-		second_item.save()
-		
-
-		saved_list = List.objects.first()
-		self.assertEqual(saved_list, list_)
-
-
-		saved_items = Item.objects.all()
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-
-		self.assertEqual(first_saved_item.list, list_)
-		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-		self.assertEqual(first_saved_item.list, list_)
-		self.assertEqual(second_saved_item.text, 'Item the second')
-		
 
 class ListViewTest(TestCase):
 	def test_uses_list_template(self):
