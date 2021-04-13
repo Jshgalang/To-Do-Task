e7e71b284698b1e879497cd42b95f66d297c5d45
isaac-gocuyo <isaac.gocuyo.official@gmail.com>
2021-04-13 08:38:51 -0400

    new form for list items

--- lists/forms.py
+++ lists/forms.py
@@ -4,9 +4,10 @@ from lists.models import Item
 # class ItemForm(forms.Form):
 	# item_text = forms.CharField(widget=forms.fields.TextInput(attrs={'placeholder':'Enter a to-do item', 'class': 'form-control input-lg'}),)
 
-
+EMPTY_ITEM_ERROR = "You can't have an empty list item"
 class ItemForm(forms.models.ModelForm):
 	class Meta:
-		model = item_text
+		model = Item
 		fields = ('text',)
-
+		widgets = {'text': forms.fields.TextInput(attrs={'placeholder':'Enter a to-do item', 'class': 'form-control input-lg'},),}
+		error_messages = {'text': {'required': EMPTY_ITEM_ERROR}}
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
 
 class ItemFormTest(TestCase):
@@ -8,3 +8,9 @@ class ItemFormTest(TestCase):
 		# self.fail(form.as_p())
 		self.assertIn('placeholder="Enter a to-do item', form.as_p())
 		self.assertIn('class="form-control input-lg"', form.as_p())
+
+	def test_form_validation_for_blank_items(self):
+		form = ItemForm(data={'text': ''})
+		self.assertFalse(form.is_valid())
+		self.assertEqual(form.errors['text'],[EMPTY_ITEM_ERROR])
+		# form.save()
