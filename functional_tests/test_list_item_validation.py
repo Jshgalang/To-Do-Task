8deb4b7b35aa8d780b569deea07ed5e022f0bc01
jshgalang <galang.joshaaron@gmail.com>
2021-04-16 20:34:54 +0800

    implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -105,3 +105,19 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy Milk')
         self.wait_for_row_in_list_table('2: Drink Red Horse Ice Cold')
+
+    def test_cannot_add_duplicate_items(self):
+        # Mike goes to homepage, starts new list
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy wellies')
+
+        # Mike accidentally tries to enter a duplicate item
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        # Mike reads a helpful error message
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element_by_css_selector(
+                    '.has-error').text, "You've already go this in your list"))
--- lists/models.py
+++ lists/models.py
@@ -4,7 +4,14 @@ from django.core.urlresolvers import reverse
 # Create your models here.
 class Item(models.Model):
 	text = models.TextField(default='')
+	# text = models.TextField(default='', unique=True)
 	list = models.ForeignKey('List', default=None)
+	class Meta:
+		ordering = ('id',)
+		unique_together = ('list', 'text')
+
+	def __str__(self):
+		return self.text
 
 class List(models.Model): # # research more about the magic in models.Model why it can be instantiated automatically
 	def get_absolute_url(self):
