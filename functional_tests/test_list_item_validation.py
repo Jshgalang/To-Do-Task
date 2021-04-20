64866945c573d2ac2858d944790d606ad97f5373
jshgalang <galang.joshaaron@gmail.com>
2021-04-20 19:32:17 +0800

    new FT and utility func

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -56,6 +56,9 @@ class ItemValidationTest(FunctionalTest):
     #     self.wait_for_row_in_list_table('1: Buy Milk')
     #     self.wait_for_row_in_list_table('2: Drink Red Horse Ice Cold')
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # Mike goes to the home page and accidentally tries to submit
         # an empty list item. He hits ENTER on the empty input box
@@ -119,5 +122,25 @@ class ItemValidationTest(FunctionalTest):
         # Mike reads a helpful error message
         self.wait_for(
             lambda: self.assertEqual(
-                self.browser.find_element_by_css_selector(
-                    '.has-error').text, "You've already got this on your list"))
+                self.get_error_element(), "You've already got this on your list"))
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Mike starts a lsit and ignites a validation error
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Payongayong')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Payongayong')
+        self.get_item_input_box().send_keys('Payongayong')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for(
+            lambda: self.assertTrue(
+                self.get_error_element().is_displayed()
+            ))
+
+        # Mike starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+        # Mike sees error message disappear
+        self.wait_for(
+            lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
+            )) #  checks element if visible
